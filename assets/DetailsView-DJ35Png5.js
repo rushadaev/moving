96007bdcb7541dc10be4c44fd80335a374bd8eca
rushadaev <rushadaev@users.noreply.github.com>
2025-01-09 22:22:07 +0000

    deploy: bcbe573b300255edd06dc19864c6a8e52de4e506

---
+++ assets/DetailsView-DJ35Png5.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,a,o}from"./index-DJe2jEmi.js";const c={},n={class:"details"};function r(i,e){return o(),t("div",n,e[0]||(e[0]=[a("h1",null,"This is details page",-1)]))}const _=s(c,[["render",r]]);export{_ as default};
