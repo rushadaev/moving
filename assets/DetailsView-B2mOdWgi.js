6dbb5de2894b4727b5fa246a44954d057d958533
rushadaev <rushadaev@users.noreply.github.com>
2025-01-10 23:26:02 +0000

    deploy: 550799a94717c088f751d5fa0936dab7d6c79cb5

--- assets/DetailsView-B2mOdWgi.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,a,o}from"./index-3po_E4xY.js";const c={},n={class:"details p-[5px"};function r(i,e){return o(),t("div",n,e[0]||(e[0]=[a("h1",null,"This is details page",-1)]))}const _=s(c,[["render",r]]);export{_ as default};
