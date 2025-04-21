00cf8df89aa9776929abc5c544b4fd21b4760c16
rushadaev <rushadaev@users.noreply.github.com>
2025-04-21 17:57:10 +0000

    deploy: 8f2a95fb3a5ba75b45021257328a607b28cdc88e

---
+++ assets/DetailsView-PrVETt3H.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,a,o}from"./index-l0cmZf3Q.js";const c={},n={class:"details p-[5px"};function r(i,e){return o(),t("div",n,e[0]||(e[0]=[a("h1",null,"This is details page",-1)]))}const _=s(c,[["render",r]]);export{_ as default};
