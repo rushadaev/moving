ac0aa6964437ef3021a01e9cad2793db671944fe
rushadaev <rushadaev@users.noreply.github.com>
2025-04-21 23:19:54 +0000

    deploy: 7b564d5f17acd93aeccea6e9a36d28ce897dba43

--- assets/TrackingView-C5KPIZNA.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,a,o as c}from"./index-l0cmZf3Q.js";const n={},r={class:"tracking p-[5px"};function o(i,e){return c(),t("div",r,e[0]||(e[0]=[a("h1",null,"This is tracking page",-1)]))}const l=s(n,[["render",o]]);export{l as default};
